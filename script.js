2ffd6caf96577ff3322e0c26c396c45a4217a932
Anna Ikoki <ikokianna@gmail.com>
2021-06-25 12:05:01 +0200

    Create square

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-// check for document.readyState to determine whether or not you need to listen for event
-// challenge: domcontentloaded used when script is inside html
-if( document.readyState !== 'loading' ) {
-  // console.log( 'document is already ready, just execute code here' );
-  myInitCode();
-} else {
-  document.addEventListener('DOMContentLoaded', function () {
-      // console.log( 'document was not ready, place code here' );
-      myInitCode();
-  });
-}
-
-function myInitCode() {
-
+// (function () {
   // set up objects to utilize content from players
   let box = {};
   
@@ -35,4 +22,41 @@ function myInitCode() {
     y:Math.floor(gameArea.height/100) //y=2
   };
 
-}  
+// trigger build of grid after page has loaded.
+// similar to document.ready object in jquery 
+// document.addEventListener('DOMContentLoaded', build);
+// check for document.readyState to determine whether or not you need to listen for event
+// challenge: domcontentloaded used when script is inside html
+// if( document.readyState !== 'loading' ) {
+//   // console.log( 'document is already ready, just execute code here' );
+//   init();
+// } else {
+//   document.addEventListener('DOMContentLoaded', function () {
+//       // console.log( 'document was not ready, place code here' );
+//   init();
+//   });
+// }
+
+build();
+// build visual part-grid and add player->box 
+function build(){
+  box = document.createElement('div');
+  // console.log(box);
+  // set class
+box.classList.add('box');
+// console.log(box);
+// add variables which wont appear but contained in object
+// start game at top left corner
+box.x = gameArea.top;
+box.y = gameArea.left;
+console.log(box);
+// apply style to add visuals
+// movable numbers as player move the box
+box.style.top=box.y + 'px';
+box.style.left=box.x + 'px';
+// console.log(box);
+// add to game area elements to add to the page
+gameAreaEle.appendChild(box);
+
+}
+// })();
